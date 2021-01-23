5ddcdc4efc33790a7eed4dcd1436581547b41f0f
Maksym Shamanskyi <m.shamanskyi91@gmail.com>
2021-01-23 14:27:36 +0200

    CRM: Move Logo to a Separate Component

--- src/components/templates/wrappers/header/Header.jsx
+++ src/components/templates/wrappers/header/Header.jsx
@@ -2,19 +2,14 @@ import React from 'react';
 
 import UnauthorizedHeaderMenu from '../../../molecules/unauthorizedHeaderMenu/UnauthorizedHeaderMenu';
 
-import Logo from '../../../../assets/logo.png';
+import Logo from '../../../atoms/logo/Logo';
 
-export default function Header({
-  goToHomePage
-}) {
+export default function Header() {
 
   return (
     <header className="header">
       <section className="header__wrapper container">
-        <div className="header__logo" onClick={goToHomePage}>
-          <img src={Logo} alt="Logo" />
-          <span>GithubCRM</span>
-        </div>
+        <Logo />
         <div className="header__controls-panel">
           <UnauthorizedHeaderMenu />
         </div>
--- src/components/templates/wrappers/header/HeaderComponent.js
+++ src/components/templates/wrappers/header/HeaderComponent.js
@@ -2,18 +2,9 @@ import React from 'react';
 
 import Header from './Header';
 
-import { history } from '../../../../configuration/history';
-import routing from '../../../../configuration/routing';
-
 export default function HeaderComponent() {
 
-  const goToHomePage = () => {
-    history.push(routing().home);
-  }
-
   return (
-    <Header
-      goToHomePage={goToHomePage}
-    />
+    <Header />
   )
 }
