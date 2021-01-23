584ae8f4556b6913ef86b0b84d741ee6157df54a
Maksym Shamanskyi <m.shamanskyi91@gmail.com>
2021-01-23 14:51:00 +0200

    CRM: Add SignIn Container

--- src/components/templates/signInPage/SignInPage.jsx
+++ src/components/templates/signInPage/SignInPage.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-export default function SignInPage() {
+import PageTitle from '../../atoms/pageTitle/PageTitle';
+
+export default function SignInPage({
+  handleSubmit = () => console.log('handle submit!')
+}) {
 
   return (
     <main className="sign-in-page">
       <section className="container">
-        SIGN IN!!!!
+        <PageTitle>
+          Sign In
+        </PageTitle>
+        <form className="sign-in-page__login-block" onSubmit={handleSubmit}>
+
+        </form>
       </section>
-    </main>
+    </main >
   )
 }
