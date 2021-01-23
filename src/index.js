f3bccc278d171a7be94623f429b7664e4d1d6598
Maksym Shamanskyi <m.shamanskyi91@gmail.com>
2021-01-23 13:25:17 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import './styles/index.scss'
+import './styles/common/_reset.scss'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
